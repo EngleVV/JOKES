ee21cf126efbadfe7b8e1ce2ff1aa99f0f8d4ee0
luwei <892244214@qq.com>
2018-12-04 22:57:01 +0800

    完成列表的展示，下一步添加下拉刷新和上拉加载更多

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
-
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -17,9 +16,7 @@ class MyApp extends StatelessWidget {
           child: new RandomWords(),
         ),
       ),
-      theme: new ThemeData(
-        primaryColor: Colors.red
-      ),
+      theme: new ThemeData(primaryColor: Colors.red),
     );
   }
 }
@@ -29,16 +26,19 @@ class RandomWordsState extends State<RandomWords> {
 
   final _saved = new Set<WordPair>();
 
-  final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _biggerFont = const TextStyle(fontSize: 10.0);
+
+  @override
+  void initState() {
+    _asyncGetSuggestions();
+  }
 
   @override
   Widget build(BuildContext context) {
 
-    setData();
-
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('Startup Name Generator'),
+        title: new Text('糗事百科'),
         actions: <Widget>[
           new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
         ],
@@ -48,32 +48,28 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   setData() async {
-    _suggestions = await _asyncGetSuggestions();    //getData()延迟执行后赋值给data
+    _suggestions = await _asyncGetSuggestions(); //getData()延迟执行后赋值给data
   }
 
   Widget _buildSuggestions() {
-    if(_suggestions.length <= 0) {
+    if (_suggestions.length <= 0) {
       return new Text("无内容");
     }
 
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();
-        final index = i ~/ 2;
-
-        return _buildRow(_suggestions[index]);
-      },
-    );
+        itemCount: _suggestions.length,
+        itemBuilder: (context, i) {
+          return _buildRow(_suggestions[i]);
+        });
   }
 
   Widget _buildRow(String content) {
-
     return new ListTile(
-      title: new Text(
-        content,
-        style: _biggerFont,
-      ),
+      title: new Card(
+          child: new Padding(
+        padding: EdgeInsets.all(10),
+        child: new Text(content),
+      )),
     );
   }
 
@@ -91,41 +87,35 @@ class RandomWordsState extends State<RandomWords> {
           ListTile.divideTiles(context: context, tiles: tiles).toList();
 
       return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Saved Suggestions')
-        ),
-          body: new ListView(children: divided)
-      );
+          appBar: new AppBar(title: new Text('Saved Suggestions')),
+          body: new ListView(children: divided));
     }));
   }
 
-  _asyncGetSuggestions() async{
+  _asyncGetSuggestions() async {
     var url = "http://m2.qiushibaike.com/article/list/text";
     var httpClient = new HttpClient();
-
-    String result;
 
     try {
       var request = await httpClient.getUrl(Uri.parse(url));
       var response = await request.close();
-      if(response.statusCode == HttpStatus.OK) {
+      if (response.statusCode == HttpStatus.OK) {
         var jsonStr = await response.transform(utf8.decoder).join();
         var data = json.decode(jsonStr);
-
 
         print(jsonStr);
         final _suggestionsA = <String>[];
         final contents = data['items'];
         contents.forEach((item) {
-          _suggestionsA.add(item);
+          _suggestionsA.add(item['content']);
         });
 
         setState(() {
           _suggestions = _suggestionsA;
         });
       }
-    } catch(excption) {
-      result = "";
+    } catch (excption) {
+      var a = 1;
     }
 
     return _suggestions;
